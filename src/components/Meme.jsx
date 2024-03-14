3127ffb46d8da9a85a859159d8d3371f0672ae6b
Nemanja <nemanja.stosicc@gmail.com>
2024-03-14 20:21:38 +0100

    update

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -9,17 +9,25 @@ export default function Meme() {
 
   const [allMemes, setAllMemes] = useState([]);
 
+  /*
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+
   useEffect(() => {
+    async function getMemesAPI() {
+      const result =  await fetch("https://api.imgflip.com/get_memes");
+      const data = await result.json();
+      setAllMemes(data.data.memes);
+    }
+
     getMemesAPI();
   }, []);
-
-  function getMemesAPI() {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((res) => res.json())
-      .then((data) => {
-        setAllMemes(data.data.memes);
-      });
-  }
 
   function handleGetMemeImage() {
     const randomIndex = Math.floor(Math.random() * allMemes.length);
